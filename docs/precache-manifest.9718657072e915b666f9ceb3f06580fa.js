1f866d874fcf3033e611a17edaad4cd4041b3c96
jcamacaro <jesus.alejandr@gmail.com>
2020-08-25 17:15:56 -0400

    README and home page

---
+++ docs/precache-manifest.9718657072e915b666f9ceb3f06580fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cdf168e1430d4d40091613b40d7746c",
+    "url": "/reactjs-20-calendar-app/index.html"
+  },
+  {
+    "revision": "a8066c283b631341b61c",
+    "url": "/reactjs-20-calendar-app/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "b11bde4b8a01160c8253",
+    "url": "/reactjs-20-calendar-app/static/css/main.27433933.chunk.css"
+  },
+  {
+    "revision": "a8066c283b631341b61c",
+    "url": "/reactjs-20-calendar-app/static/js/2.f2060f75.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/reactjs-20-calendar-app/static/js/2.f2060f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11bde4b8a01160c8253",
+    "url": "/reactjs-20-calendar-app/static/js/main.69ba3d48.chunk.js"
+  },
+  {
+    "revision": "650d16bfbb73db3a0960",
+    "url": "/reactjs-20-calendar-app/static/js/runtime-main.684bc68d.js"
+  }
+]);
